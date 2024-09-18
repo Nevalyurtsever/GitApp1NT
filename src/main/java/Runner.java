1c506839c0b984d4f6d1e6ade322ae8ee8ab3c1a
nevalYurtsever <yurtseverneval@gmail.com>
2024-09-18 22:17:30 +0200

    yeniden c3

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,10 +1,6 @@
 public class Runner {
     public static void main(String[] args) {
         System.out.println("versiyon 1");
-        System.out.println("versiyon 1");
-        System.out.println("versiyon 1 devamı");
-        System.out.println("versiyon3");
-
 
     }
 }
